755e91350f091ce6dbf5c4e837dd5e48ae2b0e8e
Hx-Ying18 <hengxuan.yingde@alumnos.upm.es>
2018-11-29 17:34:27 +0100

    Update comp_16.vhd

--- comp_16.vhd
+++ comp_16.vhd
@@ -1,49 +1,21 @@
 ----------------------------------------------------------------------------------
--- Company: 
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    18:31:47 11/08/2018 
--- Design Name: 
--- Module Name:    comp_16 - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- COMPARADOR DE 16 BITS
+-- Los comparadores son circuitos combinacionales que comparan los valores binarios
+-- de sus dos entradas de 16 bits (P y Q) y generan una salida P>Q activa a nivel alto
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity comp_16 is
-	port ( P : in STD_LOGIC_VECTOR (15 downto 0); 
-			 Q : in STD_LOGIC_VECTOR (15 downto 0);
-			 P_GT_Q : out STD_LOGIC);
+	port ( 	P : in STD_LOGIC_VECTOR (15 downto 0);	-- entrada a comparar
+		Q : in STD_LOGIC_VECTOR (15 downto 0); 	-- senal de referencia
+		P_GT_Q : out STD_LOGIC);		-- resultado de la comparacion, activa a nivel alto
 end comp_16;
 
 architecture Behavioral of comp_16 is
 begin
-	 P_GT_Q <= '1' when P > Q else
-				  '0' when P <= Q;
+	 P_GT_Q <= '1' when P > Q else -- Asignacion de la salida en funcion del resultado de la comparacion
+		   '0' when P <= Q;
 end Behavioral;
-
-
-
