8a323bfc420e4ec30b15bfb638b87cee6b81889f
Hx-Ying18 <hengxuan.yingde@alumnos.upm.es>
2018-11-29 16:40:52 +0100

    Update MUX4x8.vhd

--- MUX4x8.vhd
+++ MUX4x8.vhd
@@ -1,36 +1,14 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    17:22:07 10/11/2018 
--- Design Name: 
--- Module Name:    MUX4x8 - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- MULTIPLEXOR DE 4 ENTRADAS DE 8 BITS
+-- un mux de 8 que varíe las cuatro entradas en el display, para representar 
+-- la rotación en el display, siguiendo la descripción de la entity que aparece en 
+-- el enunciado. En el tb se comprueba que siendo las señales de entrada “0”, “1”, 
+-- “2” y “3” en binario, dependiendo de s, se muestra una u otra.
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 
 entity MUX4x8 is
     Port ( E0 : in   STD_LOGIC_VECTOR (7 downto 0);  -- entrada E0
@@ -45,7 +23,7 @@ end MUX4x8;
 architecture a_MUX4x8  of MUX4x8 is
 begin
 
-Y <= E0 when S="00" else  -- se selecciona la salida en funciÃ³n de las entradas
+Y <= E0 when S="00" else  -- se selecciona la salida en funcion de las entradas
      E1 when S="01" else  -- de control
      E2 when S="10" else
      E3 when S="11";	  
