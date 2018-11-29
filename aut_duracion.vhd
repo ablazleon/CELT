edfef4bab74c3a03704492c016de545a2fa51fd2
Hx-Ying18 <hengxuan.yingde@alumnos.upm.es>
2018-11-29 17:24:59 +0100

    Update aut_duracion.vhd

--- aut_duracion.vhd
+++ aut_duracion.vhd
@@ -1,57 +1,38 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    21:04:41 11/20/2018 
--- Design Name: 
--- Module Name:    aut_duracion - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- AUTOMATA  DE MEDIDA DE DURACION
+-- Este aut贸mata toma como entrada la salida del detector de flancos y genera varias 
+-- salidas: DURACI脫N (registro de 16 bits que almacena el n煤mero de ciclos de reloj 
+-- contados), DATO (valor binario que indica si se trata de un intervalo a 0 o a 1) 
+-- y VALID (se帽al de validaci贸n que indica que el intervalo en cuesti贸n ha terminado 
+-- (al llegar un flanco) y por lo tanto que la cuenta ha terminado. Esta se帽al dura 
+-- un ciclo de reloj).
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity aut_duracion is
 	Port( CLK_1ms  : in STD_LOGIC;    -- reloj de 1ms
-				ENTRADA  : in STD_LOGIC;    -- l韓ea de entrada de datos
-				VALID    : out STD_LOGIC;   -- salida de validaci髇 de dato
+				ENTRADA  : in STD_LOGIC;    -- l铆nea de entrada de datos
+				VALID    : out STD_LOGIC;   -- salida de validaci贸n de dato
 				DATO	   : out STD_LOGIC;	-- salida de dato ( 0 o 1)
 				DURACION : out STD_LOGIC_VECTOR (15 downto 0)); -- salida de duracion del dato
 end aut_duracion;
 
-architecture a_aut_duracion of aut_duracion is
+architecture a_aut_duracion of aut_duracion is	
 	
 	-- Declaracion de los estados de la maquina
 	type STATE_TYPE is (CERO, ALM_CERO, VALID_CERO, UNO, ALM_UNO, VALID_UNO, VALID_FIN);
 	
-	-- Declaracion de las se馻les necesarias
+	-- Declaracion de las se帽ales necesarias
 	signal ST : STATE_TYPE := CERO;
 	signal cont : STD_LOGIC_VECTOR (15 downto 0) := "0000000000000000";
 	signal reg : STD_LOGIC_VECTOR (15 downto 0) := "0000000000000000";
 
 	begin
 
-	process (CLK_1ms) -- aut髆ata de Moore: el estado depende de las entradas
+	process (CLK_1ms) -- aut贸mata de Moore: el estado depende de las entradas
 	 begin
 	 if (CLK_1ms'event and CLK_1ms='1') then
 	  case ST is
@@ -105,12 +86,11 @@ architecture a_aut_duracion of aut_duracion is
 	end process;
 	
 	-- Parte Combinacional
-	
-	VALID <= '1' when (ST = VALID_CERO or ST = VALID_UNO or ST = VALID_FIN)
+	VALID <= '1' when (ST = VALID_CERO or ST = VALID_UNO or ST = VALID_FIN) -- Asignacion de salida en funcion del estado
 				else '0';
-	DATO <=  '1' when (ST = UNO or ST = ALM_UNO or ST = VALID_UNO)
+	DATO <=  '1' when (ST = UNO or ST = ALM_UNO or ST = VALID_UNO)		-- Asignacion de salida en funcion del estado
 				else '0';
-	DURACION <= reg;
+	DURACION <= reg;							-- Asignacion de duracion medida
 
 end a_aut_duracion;
 
