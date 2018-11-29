22207a4d18009c85b7ea3fc09261bf75fcedc23a
Hx-Ying18 <hengxuan.yingde@alumnos.upm.es>
2018-11-29 17:17:25 +0100

    Update aut_control.vhd

--- aut_control.vhd
+++ aut_control.vhd
@@ -1,50 +1,28 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    17:45:52 11/15/2018 
--- Design Name: 
--- Module Name:    aut_control - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- AUTOMATA DE CONTROL
+-- Este autómata es el encargado de componer el código y validar su visualización 
+-- en el display cuando se recibe un ESPACIO. Su organización en estados responde 
+-- al diagrama.
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity aut_control is
-	Port ( CLK_1ms		: in STD_LOGIC;
-			 VALID		: in STD_LOGIC;
-			 DATO			: in STD_LOGIC;
-			 C0			: in STD_LOGIC;
-			 C1			: in STD_LOGIC;
-			 CODIGO		: out STD_LOGIC_VECTOR (7 downto 0);
-			 VALID_DISP	: out STD_LOGIC);
+	Port (	CLK_1ms		: in STD_LOGIC; 			-- reloj de 1 khz
+		VALID		: in STD_LOGIC; 			-- entrada de dato
+		DATO		: in STD_LOGIC; 			-- dato (0 o 1)
+		C0		: in STD_LOGIC; 			-- resultado comparador de ceros
+		C1		: in STD_LOGIC;				-- resultado comparador de unos
+		CODIGO		: out STD_LOGIC_VECTOR (7 downto 0); 	-- codigo morse obtenido
+		VALID_DISP	: out STD_LOGIC);			-- validacion del display
 end aut_control;
 
 architecture a_aut_control of aut_control is
 
-type STATE_TYPE is (ESPACIO, RESET, SIMBOLO, ESPERA);
-signal ST: STATE_TYPE := RESET;
+type STATE_TYPE is (ESPACIO, RESET, SIMBOLO, ESPERA); 	-- Los estados del autómata
+signal ST: STATE_TYPE := RESET;				-- Estado inicial del autómata
 
 signal s_ncod : STD_LOGIC_VECTOR (2 downto 0):="000";
 signal s_cod  : STD_LOGIC_VECTOR (4 downto 0):="00000";
@@ -58,7 +36,7 @@ process (CLK_1ms)
 		case ST is
 			when SIMBOLO =>
 				s_ncod<=s_ncod+1;
-				s_cod(n)<=C1;
+				s_cod(n)<=C1;		-- el resultado del comparador indica punto o raya
 				n<=n-1;
 				ST<=ESPERA;
 			
@@ -92,9 +70,9 @@ process (CLK_1ms)
 	end if;
 end process;
 
-VALID_DISP<= '1' when ST = ESPACIO
-				 else '0';
-CODIGO(4 downto 0)<=s_cod;
+VALID_DISP<= '1' when ST = ESPACIO 	-- Asignacion de las salidas en funcion del estado
+		else '0';
+CODIGO(4 downto 0)<=s_cod;		-- Asignacion del codigo morse
 CODIGO(7 downto 5)<=s_ncod;
 
 end a_aut_control;
