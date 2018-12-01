e0820a9b9229c657c75150b22ef4c8010e6169c9
Adrián <adrian.blazquez.leon@alumnos.upm.es>
2018-12-01 20:42:02 +0100

    Update rdesp_disp_tb.vhd

--- rdesp_disp_tb.vhd
+++ rdesp_disp_tb.vhd
@@ -24,6 +24,9 @@
 -- that these types always be used for the top-level I/O of a design in order
 -- to guarantee that the testbench will bind correctly to the post-implementation 
 -- simulation model.
+----------------------------------------------------------------------------------
+-- TESTBENCH REGISTRO DE DESPLAZAMIENTO
+--
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
